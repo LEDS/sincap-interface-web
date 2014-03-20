488c3ffc417601101de37b7a43781d040090f014
20112BSI0083 <20112BSI0083@SR6752-LEDS.cefetes.br>
2014-03-20 13:11:19 -0300

    Criação do método de busca entre datas.

---
+++ src/main/java/br/ifes/leds/sincap/web/model/BuscarPorDataForm.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ifes.leds.sincap.web.model;
+
+/**
+ *
+ * @author 20112BSI0083
+ */
+public class BuscarPorDataForm {
+    private String dataIni;
+    private String dataFim;
+
+    public String getDataIni() {
+        return dataIni;
+    }
+
+    public void setDataIni(String dataIni) {
+        this.dataIni = dataIni;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+ }
