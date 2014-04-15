8e41d19a6df26bcadf26f9e846c032d22b2f4606
20121BSI0236 <phillipe.alcantara.fonseca@gmail.com>
2014-04-14 22:36:52 -0300

    Criação do formulário para novo notificador. (Sem salvar)
    	modified:   src/main/java/br/ifes/leds/sincap/web/controller/NotificadorController.java
    	modified:   src/main/java/br/ifes/leds/sincap/web/model/InstituicaoNotificadoraForm.java
    	new file:   src/main/java/br/ifes/leds/sincap/web/model/NotificadorForm.java
    	new file:   src/main/webapp/WEB-INF/pages/form-notificador.xhtml

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificadorController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificadorController.java
@@ -7,11 +7,14 @@ package br.ifes.leds.sincap.web.controller;
 
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Notificador;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplNotificador;
+import br.ifes.leds.sincap.web.model.NotificacaoForm;
+import br.ifes.leds.sincap.web.model.NotificadorForm;
 import java.util.List;
 import javax.faces.bean.SessionScoped;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,10 +31,34 @@ public class NotificadorController {
     private AplNotificador aplNotificador;
 
     private final String urlNotificadorController = "/admin/notificador";
+    private final String urlAdicionarNotificador = "/novo";
+    private final String urlEditarNotificador = "/editar";
+    private final String urlApagarNotificador = "/apagar";
+    private final String urlSalvarNotificador = "/salvar";
 
     @RequestMapping(method = RequestMethod.GET)
     public String loadForm(ModelMap model) {
         preencherListaNotificadores(model);
+
+        return "lista-notificador";
+    }
+
+    @RequestMapping(urlAdicionarNotificador)
+    public String adicionarNotificador(ModelMap model) {
+        NotificadorForm formularioNotificador = new NotificadorForm();
+
+        model.addAttribute("formularioNotificador", formularioNotificador);
+
+        return "form-notificador";
+    }
+
+    @RequestMapping(value = urlSalvarNotificador, method = RequestMethod.POST)
+    public String salvarNotificador(@ModelAttribute NotificadorForm formularioNotificador, ModelMap model) {
+        Notificador notificador;
+
+        notificador = formularioNotificador.converterParaNotificador();
+
+        aplNotificador.salvarNotificador(notificador);
 
         return "lista-notificador";
     }
