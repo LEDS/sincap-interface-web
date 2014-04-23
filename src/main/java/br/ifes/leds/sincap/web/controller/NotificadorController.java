1b571247af5e99eca09927db38c41ffdbfeeade1
Phillipe Lopes <>
2014-04-23 16:57:58 -0300

    Finalização do cadastro de Notificador.

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificadorController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificadorController.java
@@ -5,15 +5,20 @@
  */
 package br.ifes.leds.sincap.web.controller;
 
+import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Notificador;
+import br.ifes.leds.sincap.controleInterno.cln.cgt.AplInstituicaoNotificadora;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplNotificador;
 import br.ifes.leds.sincap.web.model.NotificadorForm;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,6 +33,9 @@ public class NotificadorController {
 
     @Autowired
     private AplNotificador aplNotificador;
+
+    @Autowired
+    private AplInstituicaoNotificadora aplInstituicaoNotificadora;
 
     private final String urlNotificadorController = "/admin/notificador";
     private final String urlAdicionarNotificador = "/novo";
@@ -46,6 +54,7 @@ public class NotificadorController {
     public String adicionarNotificador(ModelMap model) {
         NotificadorForm formularioNotificador = new NotificadorForm();
 
+        preencherListaInstituicoes(model);
         model.addAttribute("formularioNotificador", formularioNotificador);
 
         return "form-notificador";
@@ -55,11 +64,56 @@ public class NotificadorController {
     public String salvarNotificador(@ModelAttribute NotificadorForm formularioNotificador, ModelMap model) {
         Notificador notificador;
 
-        notificador = formularioNotificador.converterParaNotificador();
+        notificador = formularioNotificador.converterParaNotificador(aplInstituicaoNotificadora);
 
         aplNotificador.salvarNotificador(notificador);
 
         return "redirect:" + urlNotificadorController;
+    }
+
+    @RequestMapping(value = urlEditarNotificador, method = RequestMethod.POST)
+    public String editarNotificador(@RequestBody String json, ModelMap model) {
+        Long id;
+        id = pegarParametroUnicoJSON(json);
+        
+        preencherListaInstituicoes(model);
+        
+        Notificador notificador = aplNotificador.obterNotificador(id);
+        NotificadorForm formularioNotificador = new NotificadorForm(notificador);
+        
+        model.addAttribute("formularioNotificador", formularioNotificador);
+        
+        return "form-notificador";
+    }
+    
+    @RequestMapping(value = urlApagarNotificador, method = RequestMethod.POST)
+    public String apagarNotificador(@RequestBody String json, ModelMap model) {
+        Long idNotificador;
+        idNotificador = pegarParametroUnicoJSON(json);
+        
+        aplNotificador.delete(idNotificador);
+        
+        return "redirect:" + urlNotificadorController;
+    }
+
+    private void preencherListaInstituicoes(ModelMap model) {
+        List<InstituicaoNotificadora> instituicoesNotificadoras;
+        List<SelectItem> listaInstituicoesForm = new ArrayList<>();
+
+        instituicoesNotificadoras = aplInstituicaoNotificadora.obterTodasInstituicoesNotificadoras();
+
+        for (InstituicaoNotificadora instituicao : instituicoesNotificadoras) {
+            SelectItem setorItem = new SelectItem(instituicao.getId(), instituicao.getNome());
+            listaInstituicoesForm.add(setorItem);
+        }
+
+        model.addAttribute("listaInstituicoes", listaInstituicoesForm);
+    }
+
+    public Long pegarParametroUnicoJSON(String json) {
+        String parametro = json.split("=")[1];
+
+        return Long.parseLong(parametro);
     }
 
     private void preencherListaNotificadores(ModelMap model) {
--- src/main/java/br/ifes/leds/sincap/web/model/NotificadorForm.java
+++ src/main/java/br/ifes/leds/sincap/web/model/NotificadorForm.java
@@ -5,9 +5,11 @@
  */
 package br.ifes.leds.sincap.web.model;
 
+import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Notificador;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Telefone;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.TipoTelefone;
+import br.ifes.leds.sincap.controleInterno.cln.cgt.AplInstituicaoNotificadora;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,7 +18,7 @@ import java.util.Set;
  * @author Phillipe Lopes
  */
 public class NotificadorForm {
-
+    
     private Long id;
     private String nome;
     private String cpf;
@@ -26,17 +28,18 @@ public class NotificadorForm {
     private String celular;
     private String idResidencial;
     private String residencial;
-
+    private Long[] instituicoesNotificadoras;
+    
     public NotificadorForm() {
     }
-
+    
     public NotificadorForm(Notificador notificador) {
         id = notificador.getId();
         nome = notificador.getNome();
         cpf = notificador.getCpf();
         email = notificador.getEmail();
         senha = notificador.getSenha();
-
+        
         for (Telefone telefone : notificador.getTelefones()) {
             if (telefone.getTipo() == TipoTelefone.CELULAR) {
                 idCelular = telefone.getId().toString();
@@ -46,97 +49,112 @@ public class NotificadorForm {
                 residencial = telefone.getDdd() + telefone.getNumero();
             }
         }
+        
+        instituicoesNotificadoras = new Long[notificador.getInstituicoesNotificadoras().size()];
+        Integer posicaoInstituicao = 0;
+        for (InstituicaoNotificadora instituicao : notificador.getInstituicoesNotificadoras()) {
+            instituicoesNotificadoras[posicaoInstituicao] = instituicao.getId();
+            posicaoInstituicao++;
+        }
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
     public String getCpf() {
         return cpf;
     }
-
+    
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
+    
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getSenha() {
         return senha;
     }
-
+    
     public void setSenha(String senha) {
         this.senha = senha;
     }
-
+    
     public String getCelular() {
         return celular;
     }
-
+    
     public void setCelular(String celular) {
         this.celular = celular;
     }
-
+    
     public String getResidencial() {
         return residencial;
     }
-
+    
     public void setResidencial(String residencial) {
         this.residencial = residencial;
     }
-
+    
     public String getIdCelular() {
         return idCelular;
     }
-
+    
     public void setIdCelular(String idCelular) {
         this.idCelular = idCelular;
     }
-
+    
     public String getIdResidencial() {
         return idResidencial;
     }
-
+    
     public void setIdResidencial(String idResidencial) {
         this.idResidencial = idResidencial;
     }
-
-    public Notificador converterParaNotificador() {
+    
+    public Long[] getInstituicoesNotificadoras() {
+        return instituicoesNotificadoras;
+    }
+    
+    public void setInstituicoesNotificadoras(Long[] instituicoesNotificadoras) {
+        this.instituicoesNotificadoras = instituicoesNotificadoras;
+    }
+    
+    public Notificador converterParaNotificador(AplInstituicaoNotificadora aplInstituicao) {
         Notificador notificador = new Notificador();
         Set<Telefone> telefones = new HashSet<>();
         Telefone celularTemp, residencialTemp;
-
+        
         if (id != null) {
             notificador.setId(id);
         } else {
             notificador.setActive(true);
         }
-
+        
         notificador.setEmail(email);
         notificador.setNome(nome);
         notificador.setCpf(cpf);
         notificador.setSenha(senha);
         notificador.setTipoUsuario(br.ifes.leds.sincap.controleInterno.cln.cdp.TipoUsuario.NOTIFICADOR);
-
+        
         if (celular != null) {
             celularTemp = stringParaTelefone(celular, TipoTelefone.CELULAR);
             if (idCelular != null) {
@@ -151,22 +169,30 @@ public class NotificadorForm {
             }
             telefones.add(residencialTemp);
         }
-
+        
         notificador.setTelefones(telefones);
-
+        
+        Set<InstituicaoNotificadora> instituicoes = new HashSet<>();
+        for (Long idInstituicao : instituicoesNotificadoras) {
+            InstituicaoNotificadora instituicao = aplInstituicao.obter(idInstituicao);
+            instituicoes.add(instituicao);
+        }
+        
+        notificador.setInstituicoesNotificadoras(instituicoes);
+        
         return notificador;
     }
-
+    
     private Telefone stringParaTelefone(String telefoneStr, TipoTelefone tipo) {
-
+        
         Telefone telefoneTemp = new Telefone();
-
+        
         telefoneTemp.setTipo(tipo);
-
+        
         telefoneTemp.setDdd(telefoneStr.substring(1, 3));
         telefoneTemp.setNumero(telefoneStr.substring(4, 8) + telefoneStr.substring(9, 13));
-
+        
         return telefoneTemp;
-
+        
     }
 }
