32afd881785203171034f9a8a099504f65a1b002
20121BSI0236 <phillipe.alcantara.fonseca@gmail.com>
2014-05-03 00:58:38 -0300

    Melhorias na conversão de Notificador para NotificadorDTO 	deleted:    src/main/java/br/ifes/leds/sincap/web/converter/InstituicaoNotificadoraSetToIdArrayConverter.java 	new file:   src/main/java/br/ifes/leds/sincap/web/converter/InstituicaoNotificadoraToLongId.java 	modificado: src/main/resources/dozer-bean-mappings.xml

--- src/main/java/br/ifes/leds/sincap/web/controller/AnalistaCNCDOController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/AnalistaCNCDOController.java
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.ifes.leds.sincap.web.controller;
-
 
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Funcionario;
 
@@ -13,7 +11,6 @@ import br.ifes.leds.sincap.controleInterno.cln.cdp.TipoUsuario;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplAnalistaCNCDO;
 import br.ifes.leds.sincap.web.model.AnalistaCNCDOForm;
 import br.ifes.leds.sincap.web.model.BuscarAnalistaForm;
-import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -27,34 +24,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/admin/analistacncdo")
 public class AnalistaCNCDOController {
+
     //Continuar
-    public String LoadForm()
-    {
+    public String LoadForm() {
         return "form-analistacncdoprincipal";
     }
-    
+
     @RequestMapping(value = "/buscar", method = RequestMethod.GET)
-    public String retornaAnalista(ModelMap model, BuscarAnalistaForm buscarAnalistaForm)
-    {
+    public String retornaAnalista(ModelMap model, BuscarAnalistaForm buscarAnalistaForm) {
         AplAnalistaCNCDO aplAnalistaCNCDO = new AplAnalistaCNCDO();
         Funcionario analistaCNCDO = new Funcionario();
-        AnalistaCNCDOForm analistaCNCDOForm = new AnalistaCNCDOForm();
-        try
-        {
+        AnalistaCNCDOForm analistaCNCDOForm;
+        try {
             analistaCNCDO = aplAnalistaCNCDO.obter(buscarAnalistaForm.getCpf());
-        }
-        catch(Exception e)
-        {
-            printStackTrace();
+        } catch (Exception e) {
         }
         analistaCNCDOForm = preencherForm(analistaCNCDO);
-        
+
         model.addAttribute("analistaCNCDOForm", analistaCNCDOForm);
-        
+
         return "form-analistacncdo";
-        
+
     }
-    
+
     @RequestMapping(value = "/novo", method = RequestMethod.GET)
     public String loadFormNovo(ModelMap model) {
 
@@ -64,24 +56,23 @@ public class AnalistaCNCDOController {
 
         return "form-analistacncdo";
     }
+
     @RequestMapping(value = "/novo/add", method = RequestMethod.POST)
     public String addAnalistaCNCDO(@ModelAttribute AnalistaCNCDOForm analistaCNCDOForm, ModelMap model) {
         AplAnalistaCNCDO aplAnalistaCNCDO = new AplAnalistaCNCDO();
-        
+
         Funcionario analistaCNCDO = preencherDados(analistaCNCDOForm);
-        
-        if(analistaCNCDO.getId() == null)
+
+        if (analistaCNCDO.getId() == null) {
             aplAnalistaCNCDO.salvar(analistaCNCDO);
-        
+        }
+
         model.addAttribute("analistaCNCDOForm", analistaCNCDOForm);
 
         return "form-analistacncdo";
     }
-    
-    
-    
-    public Funcionario preencherDados(AnalistaCNCDOForm analistaCNCDOForm)
-    {
+
+    public Funcionario preencherDados(AnalistaCNCDOForm analistaCNCDOForm) {
         Funcionario analistaCNCDO = new Funcionario();
         analistaCNCDO.setActive(analistaCNCDOForm.isActive());
         analistaCNCDO.setCpf(analistaCNCDOForm.getCpf());
@@ -91,19 +82,18 @@ public class AnalistaCNCDOController {
         analistaCNCDO.setTipoUsuario(TipoUsuario.ANALISTA);
         return analistaCNCDO;
     }
-    
-    public AnalistaCNCDOForm preencherForm(Funcionario analistaCNCDO)
-    {
+
+    public AnalistaCNCDOForm preencherForm(Funcionario analistaCNCDO) {
         AnalistaCNCDOForm analistaCNCDOForm = new AnalistaCNCDOForm();
         analistaCNCDOForm.setActive(analistaCNCDO.isActive());
         analistaCNCDOForm.setCpf(analistaCNCDO.getCpf());
         analistaCNCDOForm.setDocumentoSocial(analistaCNCDO.getDocumentoSocial());
         analistaCNCDOForm.setEmail(analistaCNCDO.getEmail());
         analistaCNCDOForm.setNome(analistaCNCDO.getNome());
-        
+
         //analistaCNCDOForm.setSenha(analistaCNCDO.getSenha());
         //analistaCNCDOForm.setTelefones(analistaCNCDO.get);
         return analistaCNCDOForm;
     }
-    
+
 }
