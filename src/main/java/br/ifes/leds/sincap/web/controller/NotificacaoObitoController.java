34254b41c598a9caa53018d3092529ad97c63c03
Ivana Julião <ivana_juliao@yahoo.com.br>
2014-06-27 18:34:23 -0300

    Merge

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoObitoController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoObitoController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.ifes.leds.sincap.web.controller;
 
 import javax.faces.bean.SessionScoped;
@@ -17,14 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author marcosdias
  */
 @Controller
-@RequestMapping("/obito")
+@RequestMapping(ContextUrls.APP_NOTIFICACAO_OBITO)
 @SessionScoped
-public class NotificacaoObitoControler {
+public class NotificacaoObitoController {
 
-    @RequestMapping(value = "/novo", method = RequestMethod.GET)
+    @RequestMapping(value = ContextUrls.ADICIONAR, method = RequestMethod.GET)
     public String loadFormNovaNotificacao(ModelMap model) {
-
-        
         return "form-notificacao-obito";
     }
+
 }
