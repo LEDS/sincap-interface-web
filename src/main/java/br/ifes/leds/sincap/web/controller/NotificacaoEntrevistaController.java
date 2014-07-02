5a48efbc2938cb3189ea6f7f5a2454326455a897
Breno Sampaio Grillo <bsampaio@outlook.com>
2014-07-02 17:39:55 -0300

    Criação dos métodos de controle de funcionalidades de entrevista

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoEntrevistaController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoEntrevistaController.java
@@ -3,14 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.ifes.leds.sincap.web.controller;
 
+import br.ifes.leds.reuse.ledsExceptions.CRUDExceptions.ViolacaoDeRIException;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.DTO.ProcessoNotificacaoDTO;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
 import javax.faces.bean.SessionScoped;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,16 +25,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(ContextUrls.APP_ENTREVISTA)
 @SessionScoped
 public class NotificacaoEntrevistaController {
+
     @Autowired
     AplProcessoNotificacao aplProcessoNotificacao;
-    
-    @RequestMapping( method = RequestMethod.GET)
-    public String loadFormEntrevista() {        
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String loadFormEntrevista() {
         return "form-entrevista";
     }
-    
+
     @RequestMapping(value = ContextUrls.APP_ANALISAR, method = RequestMethod.GET)
     public String loadAnalisaNotificacaoEntrevista() {
         return "analise-entrevista";
+    }
+
+    @RequestMapping(value = ContextUrls.RECUSAR, method = RequestMethod.POST)
+    public String recusarEntrevista(@ModelAttribute ProcessoNotificacaoDTO processoNotificacaoDTO,
+            ModelMap model) {
+
+        return "form-entrevista";
+    }
+
+    @RequestMapping(value = ContextUrls.SALVAR, method = RequestMethod.POST)
+    public String cadastrarEntrevista(@ModelAttribute ProcessoNotificacaoDTO processoNotificacaoDTO,
+            ModelMap model) {
+        try {
+            aplProcessoNotificacao.salvarEntrevista(processoNotificacaoDTO);
+        } catch (ViolacaoDeRIException e) {
+
+        }
+
+        return "form-entrevista";
+    }
+
+    @RequestMapping(value = ContextUrls.CONFIRMAR, method = RequestMethod.POST)
+    public String confirmarEntrevista(@ModelAttribute ProcessoNotificacaoDTO processoNotificacaoDTO,
+            ModelMap model) {
+
+        return "form-entrevista";
     }
 }
