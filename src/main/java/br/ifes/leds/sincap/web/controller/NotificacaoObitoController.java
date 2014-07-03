acb016faec6d741682148fe4da7b52ff0e6d6415
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-07-02 22:24:22 -0300

    Implementação do Controller de Notificação de Óbito.
    Foi implementada a busca pelos setores do hospital atual e a busca por
    Motivos de não doação.

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoObitoController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoObitoController.java
@@ -1,6 +1,10 @@
 package br.ifes.leds.sincap.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.ifes.leds.reuse.endereco.cgt.AplEndereco;
+import br.ifes.leds.sincap.controleInterno.cln.cdp.dto.SetorDTO;
+import br.ifes.leds.sincap.controleInterno.cln.cgt.AplCadastroInterno;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.DTO.CausaNaoDoacaoDTO;
+import br.ifes.leds.sincap.web.model.UsuarioSessao;
 import br.ifes.leds.sincap.web.utility.Utility;
 
 /**
@@ -22,12 +30,48 @@ public class NotificacaoObitoController {
 
     @Autowired
     private AplEndereco aplEndereco;
+    @Autowired
+    private AplCadastroInterno aplCadastroInterno;
+    @Autowired
+    private UsuarioSessao usuarioSessao;
+
     private Utility utility = Utility.getInstance();
 
     @RequestMapping(value = ContextUrls.ADICIONAR, method = RequestMethod.GET)
     public String loadFormNovaNotificacao(ModelMap model) {
         utility.preencherEstados(model, aplEndereco);
+
+        model.addAttribute("listaSetor", getListaSetoresSelectItem());
+        model.addAttribute("listaCausaNaoDoacao",
+                getListaCausaNDoacaoSelectItem());
+
         return "form-notificacao-obito";
+    }
+
+    private List<SelectItem> getListaCausaNDoacaoSelectItem() {
+        List<CausaNaoDoacaoDTO> listaCausas = aplCadastroInterno
+                .obterTodosCausaNaoDoacao();
+        List<SelectItem> listaCausasSelIt = new ArrayList<>();
+
+        for (CausaNaoDoacaoDTO causa : listaCausas) {
+            listaCausasSelIt
+                    .add(new SelectItem(causa.getId(), causa.getNome()));
+        }
+
+        return listaCausasSelIt;
+    }
+
+    private List<SelectItem> getListaSetoresSelectItem() {
+        List<SetorDTO> listaSetor = aplCadastroInterno
+                .obterSetorPorHospital(usuarioSessao.getIdHospital());
+
+        List<SelectItem> listaSetorString = new ArrayList<>();
+
+        for (SetorDTO setor : listaSetor) {
+            listaSetorString
+                    .add(new SelectItem(setor.getId(), setor.getNome()));
+        }
+        return listaSetorString;
     }
 
     @RequestMapping(value = ContextUrls.APP_ANALISAR, method = RequestMethod.GET)
