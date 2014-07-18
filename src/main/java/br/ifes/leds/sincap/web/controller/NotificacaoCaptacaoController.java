ff0e8f638f4cb5e2b00ce056a1889740d197a724
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-18 13:10:20 -0300

    Início do suporte a análise da captação
    O método no controlador foi criado, e também a devida página. E a
    página principal foi reajustada para exibir os processos que estão
    com a análise pendente.

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoCaptacaoController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoCaptacaoController.java
@@ -1,24 +1,48 @@
-
 package br.ifes.leds.sincap.web.controller;
 
-import javax.faces.bean.SessionScoped;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.DTO.ProcessoNotificacaoDTO;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.faces.bean.SessionScoped;
+
 /**
- *
  * @author 20102bsi0553
  */
 @Controller
 @RequestMapping(ContextUrls.APP_NOTIFICACAO_CAPTACAO)
 @SessionScoped
 public class NotificacaoCaptacaoController {
-    
+
+    @Autowired
+    AplProcessoNotificacao aplProcessoNotificacao;
+
     @RequestMapping(value = ContextUrls.ADICIONAR, method = RequestMethod.GET)
     public String loadFormCaptacap(ModelMap model) {
         return "form-notificacao-captacao";
     }
-    
+
+    /**
+     * Fornece a página para análise.
+     *
+     * @param model
+     * @param idProcesso ID do ProcessoNotificacao
+     * @return
+     */
+    @RequestMapping(value = ContextUrls.APP_ANALISAR + "/{idProcesso}", method = RequestMethod.GET)
+    public String analisar(ModelMap model, @PathVariable Long idProcesso) {
+        // Pega o processo do banco.
+        ProcessoNotificacaoDTO processo = aplProcessoNotificacao
+                .obter(idProcesso);
+
+        // Adiciona o processo ao modelo da página.
+        model.addAttribute("processo", processo);
+
+        return "analise-captacao";
+    }
 }
