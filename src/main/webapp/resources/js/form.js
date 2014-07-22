a85d119cd28c88879aede5e4506ac1dbf5244338
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-07-21 21:36:25 -0300

    Foi feito o form.js que contém utilitários para os formulários.
    O método setNames() muda o atributo *name* dos campos de
    um formulário substituindo um "-" por um ".", tornando assim
    possível mapear os atributos aninhados do formulário para o
    Spring de forma muito mais fácil.

---
+++ src/main/webapp/resources/js/form.js
@@ -0,0 +1,27 @@
+/**
+ * Created by phillipe on 21/07/14.
+ */
+
+$(document).ready(function () {
+    setNames();
+});
+
+function setNames() {
+    var replaceFunction = function (index) {
+        var input = $(this);
+        var name = input.prop("name");
+        input.prop("name", name.replace(/-/g, "."));
+    };
+
+    $("input").each(replaceFunction);
+    $("select").each(replaceFunction);
+}
+
+function definirEstilo() {
+    $(".control-group > label").addClass("control-label");
+    $(".control-group > input").addClass("span3 inline-input");
+    $(".control-group > input").wrap("<div class='controls'></div>");
+    $(".control-group > table").wrap("<div class='span3' style='padding-left: 1em'></div>");
+    $(".control-group > select").wrap("<div class='span3'></div>");
+    $("br + label").css("padding-bottom", "1em");
+}
