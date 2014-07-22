afaa8bfebf4cb4ca7307aa49445250606642d6de
Marcos Dias <marcos.a.melo.dias@gmail.com>
2014-07-22 18:54:46 -0300

    Tela de Notificacao de Captacao, e afins

--- src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoCaptacaoController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/NotificacaoCaptacaoController.java
@@ -3,12 +3,11 @@ package br.ifes.leds.sincap.web.controller;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.DTO.ProcessoNotificacaoDTO;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.EstadoNotificacaoEnum;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
+import br.ifes.leds.sincap.web.model.UsuarioSessao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.faces.bean.SessionScoped;
 import java.util.List;
@@ -23,12 +22,36 @@ public class NotificacaoCaptacaoController {
 
     @Autowired
     AplProcessoNotificacao aplProcessoNotificacao;
+    @Autowired
+    private UsuarioSessao usuarioSessao;
 
-    @RequestMapping(value = ContextUrls.ADICIONAR, method = RequestMethod.GET)
-    public String loadFormCaptacap(ModelMap model) {
-        List<ProcessoNotificacaoDTO> processos = aplProcessoNotificacao.retornarNotificacaoPorEstadoAtual(EstadoNotificacaoEnum.AGUARDANDOCAPTACAO);
+    @RequestMapping(method = RequestMethod.GET)
+    public String loadListEntrevistaAguardandoCaptacao(ModelMap model) {
+        List<ProcessoNotificacaoDTO> processos = aplProcessoNotificacao.
+                retornarNotificacaoPorEstadoAtual(EstadoNotificacaoEnum.AGUARDANDOCAPTACAO);
+
         model.addAttribute("listaProcessosNotificacao", processos);
         return "captacao";
+    }
+
+    @RequestMapping(value = ContextUrls.ADICIONAR, method = RequestMethod.POST)
+    public String loadFormCaptacao(ModelMap model, @RequestParam("id") Long id) {
+        try {
+            ProcessoNotificacaoDTO processo = aplProcessoNotificacao.obter(id);
+            model.addAttribute("processo", processo);
+
+        } catch (Exception e) {
+
+        }
+
+        return "form-notificacao-captacao";
+    }
+
+    @RequestMapping(value = ContextUrls.SALVAR, method = RequestMethod.POST)
+    public String salvarEntrevista(ModelMap model, @ModelAttribute ProcessoNotificacaoDTO processo) {
+
+        aplProcessoNotificacao.salvarCaptacao(processo, usuarioSessao.getIdUsuario());
+        return "redirect:" + ContextUrls.INDEX;
     }
 
     /**
