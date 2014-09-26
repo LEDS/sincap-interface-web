efd9df8d532d12ecdf27a208ebe0cbbab7aec7e5
André Leão <andre.sp.leao@gmail.com>
2014-09-26 13:43:44 -0300

    Criacao do metodo getBooleanMap.
    Este metodo, generico, foi criado na classe UtilityWeb com o intuito de
    preencher um campo, do tipo multiselect, na edicao de um formulario.
    Ele basicamente, preenche a lista completa como falso e depois preenche
    como verdadeiro apenas as opções escolhidas pelo usuario.
    
    Neste commit, tambem, foram feitas alteracoes na forma de exlcuir um hospital.
    Este passa a setar o atributo "ativo" como falso quando exlcuido.

--- src/main/java/br/ifes/leds/sincap/web/controller/HospitalController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/HospitalController.java
@@ -9,6 +9,7 @@ import br.ifes.leds.reuse.endereco.cdp.dto.EnderecoDTO;
 import br.ifes.leds.reuse.utility.Utility;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.BancoOlhos;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Hospital;
+import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.Setor;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplBancoOlhos;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplHospital;
@@ -19,16 +20,11 @@ import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.SelectItem;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -71,25 +67,37 @@ public class HospitalController {
     }
 
     @RequestMapping(value = ContextUrls.EDITAR + "/{idHospital}" ,method = RequestMethod.GET)
-    public String editarAnalista(ModelMap model, @PathVariable Long idHospital){
+    public String editar(ModelMap model, @PathVariable Long idHospital){
         Hospital hospital = aplHospital.obter(idHospital);
+        List<Setor> listaSetores = aplSetor.obter();
         String titulo = "hospital.editar";
         model.addAttribute("titulo", titulo);
         model.addAttribute("hospital", hospital);
         utilityWeb.preencherEndereco(mapper.map(hospital.getEndereco(), EnderecoDTO.class), model);
         utilityWeb.getBancoOlhos(model, aplBancoOlhos);
+        model.addAttribute("listaSetores",listaSetores);
         return "form-hospital";
     }
 
 
     @RequestMapping(value = ContextUrls.SALVAR, method = RequestMethod.POST)
-    public String salvar(@ModelAttribute Hospital hospital){
+    public String salvar(@ModelAttribute Hospital hospital,@RequestParam("lsetores") List<Long> lsetores){
+        Set<Setor> setSetores = new HashSet<>();
+
+        for (Long l:lsetores){
+            Setor s = new Setor();
+            s.setId(l);
+            setSetores.add(s);
+        }
+
+        hospital.setSetores(setSetores);
+
         aplHospital.cadastrar(hospital);
         return "redirect:" + ContextUrls.ADMIN + ContextUrls.APP_HOSPITAL;
     }
 
     @RequestMapping(value = ContextUrls.APAGAR + "/{idHospital}", method = RequestMethod.POST)
-    public String apagarAnalista(@PathVariable Long idHospital){
+    public String apagar(@PathVariable Long idHospital){
         Hospital hospital = aplHospital.obter(idHospital);
         aplHospital.exlcuir(hospital);
         return "redirect:" + ContextUrls.ADMIN + ContextUrls.APP_HOSPITAL;
