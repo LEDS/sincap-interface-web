76e9c8293105ab5af5238041b547af461e9ff625
Marcos Dias <marcos.a.melo.dias@gmail.com>
2014-11-03 15:48:42 -0200

    Tests sincap web, para nova captacao

---
+++ src/test/java/br/ifes/leds/sincap/web/test/CaptacaoControllerTest.java
@@ -0,0 +1,58 @@
+package br.ifes.leds.sincap.web.test;
+
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
+import lombok.SneakyThrows;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static br.ifes.leds.sincap.web.controller.ContextUrls.ADICIONAR;
+import static br.ifes.leds.sincap.web.controller.ContextUrls.APP_NOTIFICACAO_CAPTACAO;
+import static br.ifes.leds.sincap.web.controller.ContextUrls.EDITAR;
+import static br.ifes.leds.sincap.web.test.SetUp.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Marcos Dias
+ */
+public class CaptacaoControllerTest extends AbstractionTest {
+
+    private MockMvc mockMvc;
+    private MockHttpSession session;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private AplProcessoNotificacao aplProcessoNotificacao;
+
+    @Before
+    public void before() {
+        mockMvc = setUp(webApplicationContext, aplProcessoNotificacao);
+        session = criaSessao();
+    }
+
+    @Test
+    @SneakyThrows
+    public void novaCaptacaoTest() {
+        when(aplProcessoNotificacao.obter(isA(Long.class))).thenReturn(criarProcesso());
+
+        mockMvc.perform(post(APP_NOTIFICACAO_CAPTACAO + ADICIONAR)
+                .param("id", isA(Long.class).toString())
+                .session(session))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("processo", equalTo(criarProcesso())))
+                .andExpect(model().attributeExists("listaProblemasLogisticos"));
+
+        verify(aplProcessoNotificacao, times(1)).obter(isA(Long.class));
+        verifyNoMoreInteractions(aplProcessoNotificacao);
+    }
+}
