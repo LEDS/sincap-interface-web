f0b72e67fe7cd043086e19ad5e2a6049d5eeec7d
Ivana Amorim Julião <ivana_juliao@yahoo.com.br>
2014-11-04 18:20:10 -0200

    Adequação do termo de doação com o novo modelo e adição dos campos novos.

--- src/main/java/br/ifes/leds/sincap/web/controller/RelatoriosController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/RelatoriosController.java
@@ -73,7 +73,6 @@ public class RelatoriosController {
         ProcessoNotificacaoDTO pn = aplProcessoNotificacao.obter(id);
         String dataObito = utility.calendarDataToString(pn.getObito().getDataObito());
         String horaObito = utility.calendarHoraToString(pn.getObito().getDataObito());
-        String dataNascimento = utility.calendarDataToString(pn.getObito().getPaciente().getDataNascimento());
         String cidadePaciente = aplEndereco.obterCidadePorID(pn.getObito().getPaciente().getEndereco().getCidade()).getNome();
         String bairroPaciente = aplEndereco.obterBairroPorID(pn.getObito().getPaciente().getEndereco().getBairro()).getNome();
         String estadoPaciente = aplEndereco.obterEstadosPorID(pn.getObito().getPaciente().getEndereco().getEstado()).getNome();
@@ -82,6 +81,10 @@ public class RelatoriosController {
         String hospitalNome = h.getNome();
         String hospitalCidade = h.getEndereco().getCidade().getNome();
 
+        String dataEntrevista =utility.calendarDataToString(pn.getEntrevista().getDataEntrevista());
+        String horaEntrevista=utility.calendarHoraToString(pn.getEntrevista().getDataEntrevista());
+
+        //String dataNascimentoResponsavel = utility.calendarDataToString(pn.getEntrevista().getResponsavel().getDataNascimento());
         String cidadeResponsavel = aplEndereco.obterCidadePorID(pn.getEntrevista().getResponsavel().getEndereco().getCidade()).getNome();
         String bairroResponsavel = aplEndereco.obterBairroPorID(pn.getEntrevista().getResponsavel().getEndereco().getBairro()).getNome();
         String estadoResponsavel = aplEndereco.obterEstadosPorID(pn.getEntrevista().getResponsavel().getEndereco().getEstado()).getNome();
@@ -90,18 +93,29 @@ public class RelatoriosController {
             String cidadeResponsavel2 = aplEndereco.obterCidadePorID(pn.getEntrevista().getResponsavel2().getEndereco().getCidade()).getNome();
             String bairroResponsavel2 = aplEndereco.obterBairroPorID(pn.getEntrevista().getResponsavel2().getEndereco().getBairro()).getNome();
             String estadoResponsavel2 = aplEndereco.obterEstadosPorID(pn.getEntrevista().getResponsavel2().getEndereco().getEstado()).getNome();
+            String dataNascimentoResponavel2 = utility.calendarDataToString(pn.getEntrevista().getResponsavel2().getDataNascimento());
+
+            Integer idadeResponsavel2=utility.calculaIdade(pn.getEntrevista().getResponsavel2().getDataNascimento(),Calendar.getInstance());
+
             model.addAttribute("bairroResponsavel2", bairroResponsavel2);
             model.addAttribute("estadoResponsavel2", estadoResponsavel2);
             model.addAttribute("cidadeResponsavel2", cidadeResponsavel2);
+            model.addAttribute("dataNascimentoResponavel2",dataNascimentoResponavel2);
+            model.addAttribute("idadeResponsavel2",idadeResponsavel2);
         }
         String nomeFuncinario = aplFuncionario.obter(pn.getEntrevista().getFuncionario()).getNome();
 
         int idadePaciente = utility.calculaIdade(pn.getObito().getPaciente().getDataNascimento(), pn.getObito().getDataObito());
+        Integer idadeResponsavel = utility.calculaIdade(pn.getEntrevista().getResponsavel().getDataNascimento(),Calendar.getInstance());
 
         model.addAttribute("nomeFuncionario", nomeFuncinario);
         model.addAttribute("bairroResponsavel", bairroResponsavel);
         model.addAttribute("estadoResponsavel", estadoResponsavel);
         model.addAttribute("cidadeResponsavel", cidadeResponsavel);
+        //model.addAttribute("dataNascimentoResponsavel", dataNascimentoResponsavel);
+        model.addAttribute("idadeResponsavel",idadeResponsavel);
+        model.addAttribute("dataEntrevista",dataEntrevista);
+        model.addAttribute("horaEntrevista",horaEntrevista);
         model.addAttribute("hospitalNome", hospitalNome);
         model.addAttribute("hospitalCidade", hospitalCidade);
         model.addAttribute("cidadePaciente", cidadePaciente);
@@ -110,7 +124,6 @@ public class RelatoriosController {
         model.addAttribute("idadePaciente", idadePaciente);
         model.addAttribute("dataObito", dataObito);
         model.addAttribute("horaObito", horaObito);
-        model.addAttribute("dataNascimento", dataNascimento);
         model.addAttribute("pn", pn);
         //TODO: Substituir pelo endereco do formulario!
         return "form-termo-de-autorizacao";
