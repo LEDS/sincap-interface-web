437ee14337bfa4fce0c845c5c2e6b5cc3386c49a
Andre Leao <andre.sp.leao@gmail.com>
2015-02-03 14:06:57 -0200

    Acerto na exclusão das Causas de Doação.
    Anteriormente ao tentar exlcuir uma causa de não doação
    cadastrada gerava um erro. Este foi acertado e foi
    adicionado tambem um tratamento para que a causa
    so possa ser exlcuída se não houverem notificações
    vinculadas.

--- src/main/java/br/ifes/leds/sincap/web/controller/CausaNaoDoacaoController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/CausaNaoDoacaoController.java
@@ -7,6 +7,7 @@ package br.ifes.leds.sincap.web.controller;
 
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.CausaNaoDoacao;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplCausaNaoDoacao;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
 import br.ifes.leds.sincap.web.utility.UtilityWeb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import java.util.List;
 
 /**
@@ -30,12 +33,15 @@ public class CausaNaoDoacaoController {
     @Autowired
     private AplCausaNaoDoacao aplCausaNaoDoacao;
     @Autowired
+    private AplProcessoNotificacao aplProcessoNotificacao;
+    @Autowired
     private UtilityWeb utilityWeb;
     
     @RequestMapping(method = RequestMethod.GET)
     public String index(ModelMap model) {
         List<CausaNaoDoacao> causas = aplCausaNaoDoacao.obter();
         model.addAttribute("listaCausasNaoDoacao", causas);
+
         return "causa-nao-doacao";
     }
     
@@ -44,11 +50,13 @@ public class CausaNaoDoacaoController {
         String titulo = "causa-nao-doacao.cadastro";
         model.addAttribute("titulo",titulo);
         model.addAttribute("listaTiposNaoDoacao", utilityWeb.getTipoNaoDoacaoSelectItem());
+        model.addAttribute("status",false);
         return "form-causa-nao-doacao";
     }
     
     @RequestMapping(value = ContextUrls.SALVAR, method = RequestMethod.POST)
     public String salvarNovoRegistro(ModelMap model, @ModelAttribute CausaNaoDoacao causaNaoDoacao){
+        model.addAttribute("status",false);
         aplCausaNaoDoacao.cadastrar(causaNaoDoacao);
         return "redirect:" + ContextUrls.ADMIN + ContextUrls.APP_CAUSA_NAO_DOACAO;
     }
@@ -60,14 +68,27 @@ public class CausaNaoDoacaoController {
         model.addAttribute("titulo",titulo);
         model.addAttribute("listaTiposNaoDoacao", utilityWeb.getTipoNaoDoacaoSelectItem());
         model.addAttribute("causa", causa);
+        model.addAttribute("status",false);
         return "form-causa-nao-doacao";
     }
     
     @RequestMapping(value = ContextUrls.APAGAR +"/{idCausaNaoDoacao}", method = RequestMethod.POST)
     public String apagarNovoRegistro(ModelMap model, @PathVariable Long idCausaNaoDoacao){
         CausaNaoDoacao causa = aplCausaNaoDoacao.obter(idCausaNaoDoacao);
-        aplCausaNaoDoacao.excluir(causa);
-        return "redirect:" + ContextUrls.ADMIN + ContextUrls.APP_CAUSA_NAO_DOACAO;
+
+        Integer qtdNotificacoes = aplProcessoNotificacao.quantidadeCausaNaoDoacao(causa.getId());
+
+        if(qtdNotificacoes == 0) {
+            aplCausaNaoDoacao.excluir(causa);
+            List<CausaNaoDoacao> causas = aplCausaNaoDoacao.obter();
+            model.addAttribute("listaCausasNaoDoacao", causas);
+        } else {
+            List<CausaNaoDoacao> causas = aplCausaNaoDoacao.obter();
+            model.addAttribute("listaCausasNaoDoacao", causas);
+            model.addAttribute("status",true);
+            model.addAttribute("mensagem","A causa não pode ser exlcuída pois existem notificações vinculadas.");
+        }
+        return "causa-nao-doacao";
     }
 
 }
