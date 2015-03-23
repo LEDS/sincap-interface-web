6e7c2572e21d169324213ce1407dc18134ef34bd
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-03-23 10:51:21 -0300

    Notificação de óbito validando.
    Agora é necessário fazer as mensagens de erro aparecerem corretamente.

--- src/main/webapp/resources/js/form-notificacao-obito.js
+++ src/main/webapp/resources/js/form-notificacao-obito.js
@@ -50,6 +50,81 @@
         }
     });
 
+    //Validando os dados do formulario
+    var validar_form = function() {
+        $("#processo").validate({
+
+            ignore: [],
+            errorPlacement: fieldBoxValidatorError,
+            success: fieldBoxValidatorSuccess,
+            rules: {
+                //Notificacao de Obito aba Paciente
+                'obito.paciente.dataInternacao': {
+                    required: true
+                },
+                'obito.paciente.documentoSocial.tipoDocumentoComFoto': {
+                    required: true
+                },
+                'obito.paciente.sexo': {
+                    required: true
+                },
+                'obito.paciente.numeroProntuario': {
+                    required: true
+                },
+                dataObito: {
+                    required: true
+                },
+                horarioObito: {
+                    required: true,
+                    minlength: 5
+                },
+                'obito.setor': {
+                    required: true
+                },
+                'obito.primeiraCausaMortis': {
+                    required: true
+                },
+                'obito.aptoDoacao': {
+                    required: true
+                }
+            },
+            messages: {
+                //Notificacao de Obito aba Paciente
+                'obito.paciente.dataInternacao': {
+                    required: "Por favor, insira a data de internação do paciente"
+                },
+                'obito.paciente.documentoSocial.tipoDocumentoComFoto': {
+                    required: "Por favor, insira um documento social do paciente"
+                },
+                'obito.paciente.sexo': {
+                    required: "Por favor, selecione o sexo do paciente"
+                },
+                'obito.paciente.numeroProntuario': {
+                    required: "Por favor, insira o número do prontuário do paciente"
+                },
+                //Notificacao de Obito aba Obito
+                dataObito: {
+                    required: "Por favor, insira a data do óbito do paciente"
+                },
+                horarioObito: {
+                    required: "Por favor, insira o horário do óbito do paciente"
+                },
+                'obito.setor': {
+                    required: "Por favor, insira setor onde ocorreu o óbito"
+                },
+                'obito.primeiraCausaMortis': {
+                    required: "Por favor, insira primeiro motivo do obito"
+                },
+                'obito.aptoDoacao': {
+                    required: "Por favor, selecione o estado do paciente"
+                }
+            },
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+    };
+
     $(document).ready(function () {
         $('#causaNaoDoacao').select2();
         $('.data').inputmask("dd/mm/yyyy", {placeholder: "_"});
@@ -76,143 +151,6 @@
                 $("#obito-paciente-numeroSUS").rules('add', {required: true});
             }
         });
-    });
-
-    $("#form-setor").validate({
-        rules: {
-            'nome': {
-                required: true
-            }
-        },
-        messages: {
-            'nome': {
-                required: "Por favor, insira o nome"
-            }
-        },
-        submitHandler: function (form) {
-            form.submit();
-        }
-    });
-
-//Validando os dados do formulario
-    $("#processo").validate({
-
-        ignore: [],
-        errorPlacement: fieldBoxValidatorError,
-        success: fieldBoxValidatorSuccess,
-        rules: {
-            //Notificacao de Obito aba Paciente
-            'obito.paciente.nome': {
-                required: true
-            },
-            dataNascimento: {
-                required: true
-            },
-            'obito.paciente.dataInternacao': {
-                required: true
-            },
-            'obito.paciente.documentoSocial.documento': {
-                required: true
-            },
-            'obito.paciente.documentoSocial.tipoDocumentoComFoto': {
-                required: true
-            },
-            'obito.paciente.sexo': {
-                required: true
-            },
-            'obito.paciente.numeroSUS': {
-                required: true
-            },
-            'obito.paciente.numeroProntuario': {
-                required: true
-            },
-            dataObito: {
-                required: true
-            },
-            horarioObito: {
-                required: true,
-                minlength: 5
-            },
-            'obito.setor': {
-                required: true
-            },
-            'obito.primeiraCausaMortis': {
-                required: true
-            },
-            'obito.segundaCausaMortis': {
-                required: true
-            },
-            'obito.aptoDoacao': {
-                required: true
-            }
-        },
-        messages: {
-            //Notificacao de Obito aba Paciente
-            'obito.paciente.nome': {
-                required: "Por favor, insira o nome do paciente"
-            },
-            dataNascimento: {
-                required: "Por favor, insira a data de nascimento do paciente"
-            },
-            'obito.paciente.dataInternacao': {
-                required: "Por favor, insira a data de internação do paciente"
-            },
-            'obito.paciente.documentoSocial.documento': {
-                required: "Por favor, insira um documento social do paciente"
-            },
-            'obito.paciente.documentoSocial.tipoDocumentoComFoto': {
-                required: "Por favor, insira um documento social do paciente"
-            },
-            'obito.paciente.telefone.numero': {
-                required: "Por favor, insira um telefone do paciente",
-                rangelength: "O telefone de ter entre 10 e 11 dígitos"
-            },
-            'obito.paciente.sexo': {
-                required: "Por favor, selecione o sexo do paciente"
-            },
-            'obito.paciente.numeroSUS': {
-                required: "Por favor, insira o número de SUS do paciente"
-            },
-            'obito.paciente.profissao': {
-                required: "Por favor, insira a profissão do paciente"
-            },
-            'obito.paciente.nacionalidade': {
-                required: "Por favor, insira a nascionalidade do paciente"
-            },
-            'obito.paciente.numeroProntuario': {
-                required: "Por favor, insira o número do prontuário do paciente"
-            },
-            'obito.paciente.endereco.cidade': {
-                required: "Por favor, insira a cidade do paciente"
-            },
-            'obito.paciente.endereco.bairro': {
-                required: "Por favor, insira o bairro do paciente"
-            },
-            'obito.paciente.endereco.cep': {
-                required: "Por favor, insira o cep do paciente"
-            },
-            //Notificacao de Obito aba Obito
-            dataObito: {
-                required: "Por favor, insira a data do óbito do paciente"
-            },
-            horarioObito: {
-                required: "Por favor, insira o horário do óbito do paciente"
-            },
-            'obito.setor': {
-                required: "Por favor, insira setor onde ocorreu o óbito"
-            },
-            'obito.primeiraCausaMortis': {
-                required: "Por favor, insira primeiro motivo do obito"
-            },
-            'obito.segundaCausaMortis': {
-                required: "Por favor, insira segundo motivo do obito"
-            },
-            'obito.aptoDoacao': {
-                required: "Por favor, selecione o estado do paciente"
-            }
-        },
-        submitHandler: function (form) {
-            form.submit();
-        }
+        validar_form();
     });
 })();
