764c94c40fa1554112c893ef39b0c7531abc12b2
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-03-27 12:31:55 -0300

    Consulta o banco de dados de acordo com o usuário.

--- src/main/java/br/ifes/leds/sincap/web/controller/relatorios/TotalDoacaoInstituicaoCtrl.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/relatorios/TotalDoacaoInstituicaoCtrl.java
@@ -1,10 +1,14 @@
 package br.ifes.leds.sincap.web.controller.relatorios;
 
+import br.ifes.leds.sincap.controleInterno.cln.cdp.Captador;
+import br.ifes.leds.sincap.controleInterno.cln.cdp.Hospital;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
+import br.ifes.leds.sincap.controleInterno.cln.cgt.AplCaptador;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplInstituicaoNotificadora;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.relatorios.TotalDoacaoInstituicao;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplRelatorio;
 import br.ifes.leds.sincap.web.controller.ContextUrls;
+import br.ifes.leds.sincap.web.model.UsuarioSessao;
 import br.ifes.leds.sincap.web.utility.UtilityWeb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.faces.bean.SessionScoped;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -26,6 +32,8 @@ public class TotalDoacaoInstituicaoCtrl {
 
     @Autowired
     private AplInstituicaoNotificadora aplInstituicaoNotificadora;
+    @Autowired
+    private AplCaptador aplCaptador;
     @Autowired
     private UtilityWeb utilityWeb;
     @Autowired
@@ -42,7 +50,7 @@ public class TotalDoacaoInstituicaoCtrl {
     }
 
     @RequestMapping(value = ContextUrls.RLT_TOTAL_DOACAO_INSTITUICAO, method = RequestMethod.POST)
-    public String exibirRelatorio(ModelMap model,
+    public String exibirRelatorio(ModelMap model, HttpServletRequest request, HttpSession session,
 
                                   @RequestParam(value = "hospitais", required = false, defaultValue = "-1")
                                   List<Long> listaHospitais,
@@ -66,6 +74,29 @@ public class TotalDoacaoInstituicaoCtrl {
 
         model.addAttribute("listInstituicaoSelected", utilityWeb.getLongBooleanMap(instituicaoNotificadoras, listInstituicaoSelected));
 
+        if (request.isUserInRole("ROLE_ANALISTA")) {
+            adicionarInstituicoesAnalista(listaHospitais, dataInicial, dataFinal, instituicaoNotificadoras, totaisDoacaoInstituicao, hospitalNaoSelecionado);
+        } else if (request.isUserInRole("ROLE_NOTIFICADOR")){
+            final TotalDoacaoInstituicao tdi = aplRelatorio.relatorioTotalDoacaoInstituicao(((UsuarioSessao) session.getAttribute("user")).getIdHospital(), dataInicial, dataFinal);
+            totaisDoacaoInstituicao.add(tdi);
+        } else if (request.isUserInRole("ROLE_CAPTADOR")) {
+            final Captador captador = aplCaptador.obter(((UsuarioSessao) session.getAttribute("user")).getIdUsuario());
+
+            for (Hospital hospital : captador.getBancoOlhos().getHospitais()) {
+                final TotalDoacaoInstituicao tdi = aplRelatorio.relatorioTotalDoacaoInstituicao(hospital.getId(), dataInicial, dataFinal);
+                totaisDoacaoInstituicao.add(tdi);
+            }
+        }
+
+        model.addAttribute("listInstituicao", instituicaoNotificadoras);
+        model.addAttribute("listaTotaldi", totaisDoacaoInstituicao);
+
+
+        //TODO: Substituir pelo endereco do formulario!
+        return "total-doacao-instituicao";
+    }
+
+    private void adicionarInstituicoesAnalista(List<Long> listaHospitais, Calendar dataInicial, Calendar dataFinal, List<InstituicaoNotificadora> instituicaoNotificadoras, List<TotalDoacaoInstituicao> totaisDoacaoInstituicao, boolean hospitalNaoSelecionado) {
         if (hospitalNaoSelecionado) {
             for (InstituicaoNotificadora i : instituicaoNotificadoras) {
                 TotalDoacaoInstituicao tdi = aplRelatorio.relatorioTotalDoacaoInstituicao(i.getId(), dataInicial, dataFinal);
@@ -77,12 +108,5 @@ public class TotalDoacaoInstituicaoCtrl {
                 totaisDoacaoInstituicao.add(tdi);
             }
         }
-
-        model.addAttribute("listInstituicao", instituicaoNotificadoras);
-        model.addAttribute("listaTotaldi", totaisDoacaoInstituicao);
-
-
-        //TODO: Substituir pelo endereco do formulario!
-        return "total-doacao-instituicao";
     }
 }
