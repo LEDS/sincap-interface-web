a619bc884f3f3fe246e462a9f21d2695b6e1ab4c
André Aguiar <andre.s.aguiar@hotmail.com>
2015-03-31 17:27:47 -0300

    Finalizando Relatorio De Cronologia

--- src/main/java/br/ifes/leds/sincap/web/controller/RelatoriosController.java
+++ src/main/java/br/ifes/leds/sincap/web/controller/RelatoriosController.java
@@ -7,12 +7,15 @@ import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplFuncionario;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplHospital;
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplInstituicaoNotificadora;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Obito;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.ProcessoNotificacao;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.TipoNaoDoacao;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.dto.ObitoDTO;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.dto.ProcessoNotificacaoDTO;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.relatorios.*;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplRelatorio;
+import br.ifes.leds.sincap.web.annotations.DefaultTimeZone;
 import br.ifes.leds.sincap.web.model.UsuarioSessao;
 import br.ifes.leds.sincap.web.utility.UtilityWeb;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,11 +58,6 @@ public class RelatoriosController {
     AplRelatorio aplRelatorio;
 
 
-    @RequestMapping(value = RLT_POSSIBILIDADE_DOACAO_TECIDO)
-    public String rltPossibilidadeDoacaoTecido() {
-        return "possibilidade-doacao-tecido";
-    }
-
     @RequestMapping(value = ContextUrls.APP_NOTIFICACAO_ENTREVISTA + ContextUrls.RLT_TERMO_AUTORIZACAO_DOACAO, method = RequestMethod.GET)
     public String carregarFormTermoDoacao(ModelMap model) {
         List<ProcessoNotificacao> pn = aplProcessoNotificacao.obterPorPacienteNomeComEntrevistaDoacaoAutorizada("");
@@ -92,8 +90,8 @@ public class RelatoriosController {
         String hospitalNome = h.getNome();
         String hospitalCidade = h.getEndereco().getCidade().getNome();
 
-        String dataEntrevista =utility.calendarDataToString(pn.getEntrevista().getDataEntrevista());
-        String horaEntrevista=utility.calendarHoraToString(pn.getEntrevista().getDataEntrevista());
+        String dataEntrevista = utility.calendarDataToString(pn.getEntrevista().getDataEntrevista());
+        String horaEntrevista = utility.calendarHoraToString(pn.getEntrevista().getDataEntrevista());
 
         //String dataNascimentoResponsavel = utility.calendarDataToString(pn.getEntrevista().getResponsavel().getDataNascimento());
         String cidadeResponsavel = aplEndereco.obterCidadePorID(pn.getEntrevista().getResponsavel().getEndereco().getCidade()).getNome();
@@ -106,27 +104,27 @@ public class RelatoriosController {
             String estadoResponsavel2 = aplEndereco.obterEstadosPorID(pn.getEntrevista().getResponsavel2().getEndereco().getEstado()).getNome();
             String dataNascimentoResponavel2 = utility.calendarDataToString(pn.getEntrevista().getResponsavel2().getDataNascimento());
 
-            Integer idadeResponsavel2=utility.calculaIdade(pn.getEntrevista().getResponsavel2().getDataNascimento(),Calendar.getInstance());
+            Integer idadeResponsavel2 = utility.calculaIdade(pn.getEntrevista().getResponsavel2().getDataNascimento(), Calendar.getInstance());
 
             model.addAttribute("bairroResponsavel2", bairroResponsavel2);
             model.addAttribute("estadoResponsavel2", estadoResponsavel2);
             model.addAttribute("cidadeResponsavel2", cidadeResponsavel2);
-            model.addAttribute("dataNascimentoResponavel2",dataNascimentoResponavel2);
-            model.addAttribute("idadeResponsavel2",idadeResponsavel2);
+            model.addAttribute("dataNascimentoResponavel2", dataNascimentoResponavel2);
+            model.addAttribute("idadeResponsavel2", idadeResponsavel2);
         }
         String nomeFuncinario = aplFuncionario.obter(pn.getEntrevista().getFuncionario()).getNome();
 
         int idadePaciente = utility.calculaIdade(pn.getObito().getPaciente().getDataNascimento(), pn.getObito().getDataObito());
-        Integer idadeResponsavel = utility.calculaIdade(pn.getEntrevista().getResponsavel().getDataNascimento(),Calendar.getInstance());
+        Integer idadeResponsavel = utility.calculaIdade(pn.getEntrevista().getResponsavel().getDataNascimento(), Calendar.getInstance());
 
         model.addAttribute("nomeFuncionario", nomeFuncinario);
         model.addAttribute("bairroResponsavel", bairroResponsavel);
         model.addAttribute("estadoResponsavel", estadoResponsavel);
         model.addAttribute("cidadeResponsavel", cidadeResponsavel);
         //model.addAttribute("dataNascimentoResponsavel", dataNascimentoResponsavel);
-        model.addAttribute("idadeResponsavel",idadeResponsavel);
-        model.addAttribute("dataEntrevista",dataEntrevista);
-        model.addAttribute("horaEntrevista",horaEntrevista);
+        model.addAttribute("idadeResponsavel", idadeResponsavel);
+        model.addAttribute("dataEntrevista", dataEntrevista);
+        model.addAttribute("horaEntrevista", horaEntrevista);
         model.addAttribute("hospitalNome", hospitalNome);
         model.addAttribute("hospitalCidade", hospitalCidade);
         model.addAttribute("cidadePaciente", cidadePaciente);
@@ -205,19 +203,19 @@ public class RelatoriosController {
         model.addAttribute("dataInicial", dataInicial);
         model.addAttribute("dataFinal", dataFinal);
 
-        model.addAttribute("dataInicial",dataInicial);
-        model.addAttribute("dataFinal",dataFinal);
+        model.addAttribute("dataInicial", dataInicial);
+        model.addAttribute("dataFinal", dataFinal);
 
         if (lh.get(0) == -1) {
-            List<QualificacaoRecusaFamiliar> listqrf = aplRelatorio.relatorioQualificacaoRecusa(dataInicial,dataFinal,inlong);
+            List<QualificacaoRecusaFamiliar> listqrf = aplRelatorio.relatorioQualificacaoRecusa(dataInicial, dataFinal, inlong);
             model.addAttribute("listaTotalrf", listqrf);
         } else {
-            List<QualificacaoRecusaFamiliar> listqrf = aplRelatorio.relatorioQualificacaoRecusa(dataInicial,dataFinal,lh);
+            List<QualificacaoRecusaFamiliar> listqrf = aplRelatorio.relatorioQualificacaoRecusa(dataInicial, dataFinal, lh);
             model.addAttribute("listaTotalrf", listqrf);
-          }
+        }
 
 
-        model.addAttribute("listInstituicaoSelected",utilityWeb.getLongBooleanMap(in,listInstituicaoSelected));
+        model.addAttribute("listInstituicaoSelected", utilityWeb.getLongBooleanMap(in, listInstituicaoSelected));
 
         model.addAttribute("listInstituicao", in);
 
@@ -256,14 +254,14 @@ public class RelatoriosController {
     }
 
     @RequestMapping(value = ContextUrls.RLT_ATIVIDADE_MENSAL, method = RequestMethod.POST)
-    public String ExibirRelatorioAtividadeMensal(ModelMap model,HttpSession sessao, @DateTimeFormat(pattern = "MM/yyyy") @RequestParam("datMes") Calendar dataMes) {
+    public String ExibirRelatorioAtividadeMensal(ModelMap model, HttpSession sessao, @DateTimeFormat(pattern = "MM/yyyy") @RequestParam("datMes") Calendar dataMes) {
 
-        UsuarioSessao usuario = (UsuarioSessao)sessao.getAttribute("user");
+        UsuarioSessao usuario = (UsuarioSessao) sessao.getAttribute("user");
         Calendar dataInicial = Calendar.getInstance();
         Calendar dataFinal = Calendar.getInstance();
 
-        dataInicial.set(dataMes.get(Calendar.YEAR),dataMes.get(Calendar.MONTH),dataMes.get(Calendar.DAY_OF_MONTH));
-        dataFinal.set(dataMes.get(Calendar.YEAR),dataMes.get(Calendar.MONTH),dataMes.get(Calendar.DAY_OF_MONTH));
+        dataInicial.set(dataMes.get(Calendar.YEAR), dataMes.get(Calendar.MONTH), dataMes.get(Calendar.DAY_OF_MONTH));
+        dataFinal.set(dataMes.get(Calendar.YEAR), dataMes.get(Calendar.MONTH), dataMes.get(Calendar.DAY_OF_MONTH));
 
         dataInicial.set(Calendar.DAY_OF_MONTH, 1);
 
@@ -271,27 +269,65 @@ public class RelatoriosController {
         dataFinal.set(Calendar.DAY_OF_MONTH, 1);
         dataFinal.add(Calendar.DAY_OF_MONTH, -1);
 
-        List<FaixaEtaria> listaFaixa = aplRelatorio.retornaFaixaEtaria(usuario.getIdHospital(),dataInicial,dataFinal);
-        model.addAttribute("listaFaixa",listaFaixa);
+        List<FaixaEtaria> listaFaixa = aplRelatorio.retornaFaixaEtaria(usuario.getIdHospital(), dataInicial, dataFinal);
+        model.addAttribute("listaFaixa", listaFaixa);
 
-        List<ObitoCardio> liObito = aplRelatorio.retornaObitoCardio(usuario.getIdHospital(),dataInicial,dataFinal);
-        model.addAttribute("liObito",liObito);
+        List<ObitoCardio> liObito = aplRelatorio.retornaObitoCardio(usuario.getIdHospital(), dataInicial, dataFinal);
+        model.addAttribute("liObito", liObito);
 
-        List<ObitosMeTurno> listaObitosME = aplRelatorio.retornaObitosMeTurno(usuario.getIdHospital(),dataInicial,dataFinal);
-        model.addAttribute("listaObitosME",listaObitosME);
+        List<ObitosMeTurno> listaObitosME = aplRelatorio.retornaObitosMeTurno(usuario.getIdHospital(), dataInicial, dataFinal);
+        model.addAttribute("listaObitosME", listaObitosME);
 
         List<NaoDoacaoCIHDOTT> recusaFamiliar = aplRelatorio.naoDoacaoMensal(usuario.getIdHospital(), dataInicial, dataFinal, TipoNaoDoacao.RECUSA_FAMILIAR);
-        model.addAttribute("listaNaoDoacaoFamiliar",recusaFamiliar);
+        model.addAttribute("listaNaoDoacaoFamiliar", recusaFamiliar);
 
         List<NaoDoacaoCIHDOTT> problemaMedico = aplRelatorio.naoDoacaoMensal(usuario.getIdHospital(), dataInicial, dataFinal, TipoNaoDoacao.CONTRAINDICACAO_MEDICA);
-        model.addAttribute("listaProbMedico",problemaMedico);
+        model.addAttribute("listaProbMedico", problemaMedico);
 
         List<NaoDoacaoCIHDOTT> problemaEstrutural = aplRelatorio.naoDoacaoMensal(usuario.getIdHospital(), dataInicial, dataFinal, TipoNaoDoacao.PROBLEMAS_ESTRUTURAIS);
-        model.addAttribute("listaProblemaEstrutural",problemaEstrutural);
+        model.addAttribute("listaProblemaEstrutural", problemaEstrutural);
 
 
         model.addAttribute("datMes", dataMes);
 
         return "rel-atividade-mensal";
+    }
+
+    @RequestMapping(value = ContextUrls.RLT_POSSIBILIDADE_DOACAO_TECIDO, method = RequestMethod.GET)
+    public String carregarRelatorioCronologia() {
+
+        return "possibilidade-doacao-tecido";
+    }
+
+    @DefaultTimeZone
+    @RequestMapping(value = ContextUrls.RLT_POSSIBILIDADE_DOACAO_TECIDO, method = RequestMethod.POST)
+    public String exibirRelatorioCronologia(ModelMap model, @DateTimeFormat(pattern = "dd/MM/yyyy") @RequestParam("dataInicial") Calendar dataInicial, @DateTimeFormat(pattern = "dd/MM/yyyy") @RequestParam("dataFinal") Calendar dataFinal, HttpSession session) {
+
+
+        UsuarioSessao usuarioSessao = (UsuarioSessao) session.getAttribute("user");
+        long idHosp;
+        idHosp = usuarioSessao.getIdHospital();
+
+        List<Obito> obitosHosp = aplRelatorio.todosObitosPorHospital(idHosp, dataInicial, dataFinal);
+        List<ProcessoNotificacao> listaProc = new ArrayList<>();
+        List<RelatorioCronologia> listaRel = new ArrayList<>();
+
+
+        for (Obito obito : obitosHosp) {
+            listaProc.add(aplProcessoNotificacao.retornaProcesso(obito.getId()));
+        }
+
+
+
+
+        for (ProcessoNotificacao processo : listaProc) {
+            RelatorioCronologia tdi = aplRelatorio.relatorioCronologia(processo);
+            listaRel.add(tdi);
+        }
+
+        model.addAttribute("listaTotaldi", listaRel);
+
+        //TODO: Substituir pelo endereco do formulario!
+        return "possibilidade-doacao-tecido";
     }
 }
